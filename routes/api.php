e532fa0f04864af73742a9ce1a013dad31ffa3bf
Bing-Sheng Chen <gundamew@gmail.com>
2021-07-21 18:51:07 +0800

    Revert "feat: Ensure the media type of incoming request"
    This reverts commit ba92bb36f2b7eaf245264eaacba5321e0a68791b.

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/boulders', BoulderController::class)
-    ->middleware('supported');
+Route::apiResource('/boulders', BoulderController::class);
